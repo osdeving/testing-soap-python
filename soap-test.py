4d1d81df27897e7bc48f5fd5a61c152b306c969a
Willams S. de Sousa <osdeving@gmail.com>
2020-09-23 11:42:44 -0300

    Iniciando projeto

--- soap-test.py
+++ soap-test.py
@@ -2,5 +2,5 @@
 from zeep import Client
 
 client = Client('http://www.soapclient.com/xml/soapresponder.wsdl')
-reslt = client.service.Method1(bstrParam1='oi', bstrParam2='tchau')
+result = client.service.Method1(bstrParam1='oi', bstrParam2='tchau')
 print(result)
